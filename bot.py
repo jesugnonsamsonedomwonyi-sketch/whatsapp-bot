660b6a5570fe96ac86373d5c736e37956023877e
jesugnonsamsonedomwonyi-sketch <jesugnonsamsonedomwonyi@gmail.com>
2026-05-25 23:03:51 +0100

    Flask

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot is running online"
+    return "Bot running"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
