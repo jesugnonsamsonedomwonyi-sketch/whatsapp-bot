7c116fe24cbac026eca79ff33764747cfa1af946
jesugnonsamsonedomwonyi-sketch <jesugnonsamsonedomwonyi@gmail.com>
2026-05-26 11:50:02 +0100

    requirements.txt

--- bot.py
+++ bot.py
@@ -8,5 +8,4 @@ def home():
     return "Bot running"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
