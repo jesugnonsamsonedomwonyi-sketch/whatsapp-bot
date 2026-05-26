a4e96b8ee757236ed440bce5c9600e15e4fe0d0c
jesugnonsamsonedomwonyi-sketch <jesugnonsamsonedomwonyi@gmail.com>
2026-05-26 13:20:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,19 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+@app.route("/webhook", methods=["GET"])
+def verify():
+    VERIFY_TOKEN = "mybot123"
 
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode and token:
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge
+
+    return "Verification failed"
 @app.route("/")
 def home():
     return "Bot running"
